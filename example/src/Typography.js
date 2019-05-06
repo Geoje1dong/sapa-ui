4fc6baaf8554f5314d626aab16b435f6ac575f7d
Kim Jae Hyune <wogus4041@naver.com>
2019-05-06 16:53:03 +0900

    typography css 추가

--- example/src/Typography.js
+++ example/src/Typography.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import styled from 'styled-components'
+
 import {
   Typography
 } from 'sapa-ui/dist/index';
@@ -7,31 +9,61 @@ import Header from './Header';
 const styles = {
   root: {
     width: '100%',
-    maxWidth: 500,
+    maxWidth:'500px',
+    margin:'0 auto',
+    padding:'40px 0'
   },
 };
+
+const Cardbox = styled.div`
+  font-famil:'sans-serif';
+  width:500px;
+  padding-bottom:1rem;
+  p{
+    font-size:12px;
+    margin-bottom:1rem;
+  }
+`
 
 const TypographyPage = props => (
   <div style={styles.root}>
     <Header />
-    <Typography variant="h1">
-      h1. Heading
-    </Typography>
-    <Typography variant="h2">
-      h2. Heading
-    </Typography>
-    <Typography variant="h3">
-      h3. Heading
-    </Typography>
-    <Typography variant="h4">
-      h4. Heading
-    </Typography>
-    <Typography variant="h5">
-      h5. Heading
-    </Typography>
-    <Typography variant="h6">
-      h6. Heading
-    </Typography>
+    <Cardbox>
+      <p>Header1 48px</p>
+      <Typography variant="h1">
+        직관 적인 디자인 시스템<br />SAPA UI
+      </Typography>
+    </Cardbox>
+    <Cardbox>
+      <p>Header2 36px</p>
+      <Typography variant="h2">
+        직관 적인 디자인 시스템<br />SAPA UI
+      </Typography>
+    </Cardbox>
+    <Cardbox>
+      <p>Header3 32px</p>
+      <Typography variant="h3">
+        직관 적인 디자인 시스템<br />SAPA UI
+      </Typography>
+    </Cardbox>
+    <Cardbox>
+      <p>Header4 28px</p>
+      <Typography variant="h4">
+        직관 적인 디자인 시스템<br />SAPA UI
+      </Typography>
+    </Cardbox>
+    <Cardbox>
+      <p>Header5 24px</p>
+      <Typography variant="h5">
+        직관 적인 디자인 시스템<br />SAPA UI
+      </Typography>
+    </Cardbox>
+    <Cardbox>
+      <p>Header6 20px</p>
+      <Typography variant="h6">
+        직관 적인 디자인 시스템<br />SAPA UI
+      </Typography>
+    </Cardbox>
   </div>
 )
 
--- src/typography.js
+++ src/typography.js
@@ -1,21 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
 
 export default class Typography extends React.Component {
 
   _getElementByVariant = (variant, children) => {
     switch (variant) {
       case 'h1':
-        return (<h1>{children}</h1>)
+        return (<H1>{children}</H1>)
       case 'h2':
-        return (<h2>{children}</h2>)
+        return (<H2>{children}</H2>)
       case 'h3':
-        return (<h3>{children}</h3>)
+        return (<H3>{children}</H3>)
       case 'h4':
-        return (<h4>{children}</h4>)
+        return (<H4>{children}</H4>)
       case 'h5':
-        return (<h5>{children}</h5>)
+        return (<H5>{children}</H5>)
       case 'h6':
-        return (<h6>{children}</h6>)
+        return (<H6>{children}</H6>)
     }
   }
   
@@ -30,3 +31,32 @@ export default class Typography extends React.Component {
     )
   }
 }
+
+const H1 = styled.h1`
+font-size:3rem;
+margin-bottom:1rem;
+`
+const H2 = styled.h2`
+font-size:2.25rem;
+margin-bottom:1rem;
+`
+
+const H3 = styled.h3`
+font-size:2rem;
+margin-bottom:1rem;
+`
+
+const H4 = styled.h4`
+font-size:1.75rem;
+margin-bottom:1rem;
+`
+
+const H5 = styled.h5`
+font-size:1.5rem;
+margin-bottom:1rem;
+`
+
+const H6 = styled.h6`
+font-size:1.25rem;
+margin-bottom:1rem;
+`
